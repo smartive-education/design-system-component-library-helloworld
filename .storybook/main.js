f0c1d1014289e92b57d369521669b6b0a6397be4
Caro <carole.hug@outlook.com>
2022-11-29 20:22:07 +0100

    initialize storybook

---
+++ .storybook/main.js
@@ -0,0 +1,28 @@
+module.exports = {
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true)
+    }
+  },
+  stories: [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  addons: [
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        cssLoaderOptions: {
+          importLoaders: 1
+        },
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        }
+      }
+    }
+  ]
+};
