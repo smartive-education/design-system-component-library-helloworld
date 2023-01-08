8485947ff6b021b31756f3e1039c4a0730c365c6
Caro <carole.hug@outlook.com>
2023-01-08 16:14:20 +0100

    run prettier

--- src/components/atoms/comment-button/comment-button.tsx
+++ src/components/atoms/comment-button/comment-button.tsx
@@ -1,22 +1,30 @@
-import React, {FC} from 'react';
+import React, { FC } from 'react';
 import InteractionButton from '../interaction-button/interaction-button';
 import MessageIconSvGComponent from '../icons/message-icon';
 
 export type LikeWithReactionProps = {
-    label: {
-        noComments: string;
-        someComments: string;
-    },
-    numberOfComments: number,
+  label: {
+    noComments: string;
+    someComments: string;
+  };
+  numberOfComments: number;
 };
 
-const CommentButton: FC<LikeWithReactionProps> = ({  label, numberOfComments }) => {
-    let active = numberOfComments > 0;
+const CommentButton: FC<LikeWithReactionProps> = ({ label, numberOfComments }) => {
+  const active = numberOfComments > 0;
 
-    return <InteractionButton label={active ? numberOfComments + ' ' + label.someComments : label.noComments} colorVariant={'violet'} active={active}>
-        <MessageIconSvGComponent variant={active ? 'filled' : 'normal'} classes={active ? 'fill-violet-600' : 'fill-slate-600 group-hover:fill-violet-600'}/>
+  return (
+    <InteractionButton
+      label={active ? numberOfComments + ' ' + label.someComments : label.noComments}
+      colorVariant={'violet'}
+      active={active}
+    >
+      <MessageIconSvGComponent
+        variant={active ? 'filled' : 'normal'}
+        classes={active ? 'fill-violet-600' : 'fill-slate-600 group-hover:fill-violet-600'}
+      />
     </InteractionButton>
+  );
 };
 
 export default CommentButton;
-
