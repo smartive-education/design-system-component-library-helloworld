495a7bdfd3f1997baffe9e84cf61d00f84c231b0
Caro <carole.hug@outlook.com>
2023-01-09 22:14:58 +0100

    refactoring

--- src/components/molecules/input/input.stories.tsx
+++ src/components/molecules/input/input.stories.tsx
@@ -1,17 +1,17 @@
-import {ComponentMeta, ComponentStory} from '@storybook/react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
 import Input from './input';
 
 export default {
-    title: 'Components/Molecules/Input',
-    component: Input
+  title: 'Components/Molecules/Input',
+  component: Input
 } as ComponentMeta<typeof Input>;
 
-const Template: ComponentStory<typeof Input> = (args) => <Input {...args}/>;
+const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
 
 export const InputComponent = Template.bind({});
 InputComponent.args = {
-    label: 'Label',
-    placeholder: 'Placeholder',
-    type: 'text'
+  label: 'Label',
+  placeholder: 'Placeholder',
+  type: 'text'
 };
--- src/components/molecules/input/input.tsx
+++ src/components/molecules/input/input.tsx
@@ -1,20 +1,28 @@
-import React, {FC} from 'react';
+import React, { FC } from 'react';
 import MumbleIconSvGComponent from '../../atoms/icon/mumble-icon';
 
 export type InputProps = {
-    label: string;
-    placeholder: string;
-    type?: 'text' | 'email' | 'number';
+  label: string;
+  placeholder: string;
+  type?: 'text' | 'email' | 'number';
 };
 
-const Input: FC<InputProps> = ({  label,  placeholder, type ='text' }) => {
-    return <div className={'block text-slate-700'}>
-        <label>{label}</label>
-        <div className={'w-264 h-48 relative'}>
-            <MumbleIconSvGComponent className="w-8 h-8 flex absolute right-s top-s bottom-s" />
-            <input type={type} placeholder={placeholder}  className={'w-264 h-48 border-1 border-slate-100 hover:border-violet-600 focus:outline focus:outline-none focus:border-2 rounded-s px-s'}></input>
-        </div>
+const Input: FC<InputProps> = ({ label, placeholder, type = 'text' }) => {
+  return (
+    <div className={'block text-slate-700'}>
+      <label>{label}</label>
+      <div className={'w-264 h-48 relative'}>
+        <MumbleIconSvGComponent className="w-8 h-8 flex absolute right-s top-s bottom-s" />
+        <input
+          type={type}
+          placeholder={placeholder}
+          className={
+            'w-264 h-48 border-1 border-slate-100 hover:border-violet-600 focus:border-violet-600 focus:outline focus:outline-none focus:border-2 rounded-s px-s'
+          }
+        ></input>
+      </div>
     </div>
+  );
 };
 
 export default Input;
