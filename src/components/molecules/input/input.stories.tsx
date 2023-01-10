0810288a40de895e67804aedfa960aed3acba756
Caro <carole.hug@outlook.com>
2023-01-10 21:02:30 +0100

    fix icon in input

--- src/components/molecules/input/input.stories.tsx
+++ src/components/molecules/input/input.stories.tsx
@@ -1,13 +1,14 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
 import Input from './input';
+import MumbleIconSvGComponent from "../../atoms/icon";
 
 export default {
   title: 'Components/Molecules/Input',
   component: Input
 } as ComponentMeta<typeof Input>;
 
-const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
+const Template: ComponentStory<typeof Input> = (args) => <Input {...args} ><MumbleIconSvGComponent /></Input>;
 
 export const InputComponent = Template.bind({});
 InputComponent.args = {
--- src/components/molecules/input/input.tsx
+++ src/components/molecules/input/input.tsx
@@ -1,18 +1,21 @@
-import React, { FC } from 'react';
+import React, {FC, ReactNode} from 'react';
 import MumbleIconSvGComponent from '../../atoms/icon/mumble-icon';
 
 export type InputProps = {
   label: string;
   placeholder: string;
   type?: 'text' | 'email' | 'number';
+  children: ReactNode;
 };
 
-const Input: FC<InputProps> = ({ label, placeholder, type = 'text' }) => {
+const Input: FC<InputProps> = ({ label, placeholder, type = 'text', children }) => {
   return (
     <div className={'block text-slate-700'}>
       <label>{label}</label>
       <div className={'w-264 h-48 relative'}>
-        <MumbleIconSvGComponent className="w-8 h-8 flex absolute right-s top-s bottom-s" />
+        <div className={'first:w-8 first:h-8 first:flex first:absolute first:right-s first:top-s first:bottom-s first:fill-slate-600'}>
+          {children}
+        </div>
         <input
           type={type}
           placeholder={placeholder}
